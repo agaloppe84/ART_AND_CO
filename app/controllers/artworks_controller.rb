84384a1412b16df71f277abd7763611cc7e47306
martin malinvaud <martin.malinvaud@gmail.com>
2016-02-23 12:20:20 +0100

    artwork controller done et refacto

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,5 +1,46 @@
 class ArtworksController < ApplicationController
+
+  before_action :set_artwork, only: [:show, :edit, :update, :destroy]
+
   validates :name, presence: true
   validates :user_id, presence: true
   validates :genre, presence: true
+
+  def index
+    @artwork = Artwork.all
+  end
+
+  def show
+  end
+
+  def edit
+    @artwork.save
+  end
+
+  def create
+    @artwork = Artwork.new(artwork_params)
+    @artwork.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  def update
+    @artwork.update(artwork_params)
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  def destroy
+    @artwork.destroy
+    redirect_to artworks_path
+  end
+
+  private
+
+  def set_artwork
+    @artwork = Artwork.find(params[:id])
+  end
+
+  def artwork_params
+    params.require(:artwork).permit(:name, :description, :price, :genre)
+  end
+
 end
