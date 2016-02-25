1857aab7411397d5bc14e406acc620d291d0bd57
aurelien galoppe <agaloppe@me.com>
2016-02-25 14:34:30 +0100

    artworks new

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -5,4 +5,8 @@ class Artwork < ActiveRecord::Base
   belongs_to :user
   has_many :reservations
   has_many :users, through: :reservations
+
+  def self.genres
+    ["painting", "sculpture", "photography"]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :users
   resources :reservations, except: [:update, :destroy]
-  resources :artworks, only: [:create, :index, :show]
+  resources :artworks
 end
