efbceff6eb4ea5cd18254565378d6306ab38ad6f
aurelien galoppe <agaloppe@me.com>
2016-02-25 18:41:09 +0100

    change apply to navbar and dashboard created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'dashboard' => 'pages#dashboard'
   resources :users
   resources :reservations, except: [:update, :destroy]
   resources :artworks
